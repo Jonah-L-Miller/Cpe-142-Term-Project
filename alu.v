768c8221bef089027bd3e2a40587d0f2fdd9c0ab
Hpayne99 <82551168+Hpayne99@users.noreply.github.com>
2021-04-18 19:48:20 -0700

    Update alu.v
    fixed non-function code logic
    added action log to document actions, dates, and users

--- alu.v
+++ alu.v
@@ -1,11 +1,10 @@
-module alu(in1, in2, out, r0, con);
+module alu(in1, in2, out, r0, con); //compiles, hPAYNE (4/18/21)
 	input [15:0] in1, in2;
 	input [3:0] con;
 	
 	output reg [15:0] out, r0;
 	
 	always@(*) begin	//falling edge of clock?
-		carry = 32'h 0000;
 		case (con)
 			4'h 1:	out = in1 + in2;	//0001
 			4'h 2:	out = in1 - in2;	//0010
@@ -15,11 +14,17 @@ module alu(in1, in2, out, r0, con);
 				out = in1 / in2;	//quotient
 				r0 = in1 % in2;		//remainder
 			end			
-			4'h C:	//1100
-			4'h D:	//1101
-			4'h E:	//1110
-			4'h F:	//1111
+			4'h C: out = in1 & in2;	//1100 ANDI
+		//	4'h D:	//1101 
+			4'h E: out = in1 | in2;	//1110 ORI
+			4'h F: out = in1 + in2;	//1111 ADD NO FUNC
 			
 		endcase
 	end
 endmodule
+/*_________________ACTION LOG_______________*/
+/*______ACTION__________DATE____________USER*/
+/*---___---___---___---___---___---___---___*/
+//	COMPILE		4-18-21		PAYNE
+//
+/*---___---___---___---___---___---___---___*/
