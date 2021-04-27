af7fc218749b3a771c513f53967f948dc9f15006
Hpayne99 <82551168+Hpayne99@users.noreply.github.com>
2021-04-27 14:31:22 -0700

    Update cpu.v
    added program_counter, if_adder

--- cpu.v
+++ cpu.v
@@ -1,13 +1,18 @@
 `include "instruction_memory.v"
+`include "program_counter.v"
+`include "adder.v"
+
 
 module cpu(
-	input clk, 
+	input clock, 
 	input reset
 );
 
 ///// FETCH STAGE WIRES /////
-    wire [15:0] if_from_pc, if_instruction;
-
+	wire [15:0] if_from_pc, if_instruction;
+	wire [15:0] if_pc_new_address, if_instruction_address, if_adder_result_address;
+	wire if_pc_stop;
+	
 
 ///// DECODE STAGE WIRES /////
 
@@ -26,12 +31,30 @@ module cpu(
 
 
 ///// FETCH STAGE /////
+
+	
+	//program counter
+	program_counter if_program_counter(
+		.pc_new_address(if_pc_new_address),				//address from IF_MUX
+		.instruction_address(if_instruction_address),	//address from PC
+		.pc_stop(if_pc_stop),							//pause pc with PC_pause or halt
+		.clock(clock),
+		.reset(reset)
+		);
+	
+	//adder
+	adder if_adder(
+		.in1(if_instruction_address),
+		.in2(16'h0002),
+		.out(if_adder_result_address
+		);
+
 	//instruction memory input and output
 	instruction_memory if_instruction_memory (
-		.if_from_pc(if_from_pc),			//adder that increments the address' memory to the next location ie. +2
+		.if_from_pc(if_from_pc),				//adder that increments the address' memory to the next location ie. +2
 		.if_instruction(if_instruction)			//instruction leaving instruction memory and going into IF/ID buffer
 	);	
-
+	
 
 ///// IF/ID BUFFER /////
 
