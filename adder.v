e77635674aacfed503cd16ad65f74cb06524629b
Hpayne99 <82551168+Hpayne99@users.noreply.github.com>
2021-04-27 14:21:10 -0700

    joint coding
    fixed adder.v, wrote program_counter with Jonah

--- adder.v
+++ adder.v
@@ -1,14 +1,8 @@
-module adder (in1, in2, out);
-	input [15:0] in1, in2;
-	
-	output reg [15:0] out;
+module adder(
+			input [15:0] in1, in2, 
+
+			output reg[15:0] out);
 	
 	always@ (*)
 		out = in1 + in2;
 endmodule
-// ACTION LOG
-// ACTION     DATE     USER
-// -----------------------------
-// COMPILE01  4-18-21  PAYNE
-//
-// --------------------
---
+++ program_counter.v
@@ -0,0 +1,17 @@
+module program_counter (
+	input [15:0] pc_new_address,
+	input pc_stop, clock, reset,
+	
+	output reg [15:0] instruction_address);
+	
+	always@(posedge clock or negedge reset) begin
+		if (reset)
+			instruction_address = 16'h 0000;
+		else begin
+			if (pc_stop)
+				instruction_address = instruction_address;
+			else
+				instruction_address = pc_new_address;
+		end
+	end
+endmodule
