b38ba224d4a54b66dafd125305e734aa693c9a94
Hpayne99 <82551168+Hpayne99@users.noreply.github.com>
2021-04-27 14:34:20 -0700

    formatting
    fixed formatting of mux2, mux3

--- mux2.v
+++ mux2.v
@@ -1,8 +1,6 @@
-module mux2(in1,in2, out, s);
-	input [15:0] in1, in2;
-	input s;
-	
-	output reg [15:0] out;
+module mux2(input [15:0] in1,in2, 
+			input s,
+			output reg [15:0] out);
 	
 	always@(*)
 		if (s)
--- mux3.v
+++ mux3.v
@@ -1,8 +1,6 @@
-module mux3(in1,in2, in3, out, s);
-	input [15:0] in1, in2, in3;
-	input [1:0] s;
-	
-	output reg [15:0] out;
+module mux3(input {15:0] in1,in2, in3
+			input [1:0] s,
+			output reg [15:0] out);
 	
 	always@(*)
 		case (s)
