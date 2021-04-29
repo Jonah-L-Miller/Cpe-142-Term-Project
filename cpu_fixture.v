ff8c5b6ec8160bc2b6d9147e802a3af549c6051f
jonah-miller <jonahlmill@gmail.com>
2021-04-29 14:38:31 -0700

    Update cpu_fixture.v

--- cpu_fixture.v
+++ cpu_fixture.v
@@ -29,7 +29,7 @@ module cpu_fixture();
     begin
     	$display("\n clock cycles = %d,   reset = %h",clock_cycles, reset);
 	//IF-----
-    $display("-----IF-----");
+		$display("-----IF-----\n");
 		$display("IF_ADDRESS_FROM_PC           = %h", CPU.if_address_from_pc);
 		$display("IF_INSTRUCTION       = %h", CPU.if_instruction);
 		$display("IF_PC_NEW_ADDRESS    = %h", CPU.if_pc_new_address);
@@ -38,12 +38,34 @@ module cpu_fixture();
 		$display("EX_IF_BRANCH_LOCATION_RESULT  	   = %h",CPU.ex_if_branch_location_result);
 		$display("IF_PC_MUX  	   = %h",CPU.if_pc_mux);
 	//ID-----
-    $display("-----ID-----");
-		$display("IF_ID_BUFFER_FLUSH       = %h",CPU.if_id_buffer_flush);
-		$display("IF_ID_BUFFER_HOLD        = %h",CPU.if_id_buffer_hold);
-		$display("ID_INSTRUCTION  	       = %h",CPU.id_intruction);
-		$display("ID_PC_NEXT_ADDRESS  	   = %h",CPU.id_pc_next_address);
-    
+		$display("-----ID-----\n");
+		$display("IF_ID_BUFFER_FLUSH       				= %h",CPU.if_id_buffer_flush);
+		$display("IF_ID_BUFFER_HOLD        				= %h",CPU.if_id_buffer_hold);
+		$display("ID_INSTRUCTION  	       				= %h",CPU.id_intruction);
+		$display("ID_PC_NEXT_ADDRESS  	   				= %h",CPU.id_pc_next_address);
+		$display("ID_OPCODE  		  	   				= %h",CPU.id_opcode);
+		$display("ID_OP1  		  	   	   				= %h",CPU.id_op1);
+		$display("ID_OP2				   				= %h",CPU.id_op2);
+		$display("ID_IMMEDIATE			   				= %h",CPU.id_immdediate);
+		$display("ID_FUNCTION_CODE         				= %h",CPU.id_function_code);
+		$display("ID_MUX2_OUTPUT		   				= %h",CPU.id_mux2_output);
+		$display("ID_MUX2_SELECTOR         				= %h",CPU.id_mux2_selector);
+		$display("WB_ID_WRITE_REG          				= %h",CPU.wb_id_write_reg);
+		$display("WB_ID_WRITE_DATA         				= %h",CPU.wb_id_write_data);
+		$display("WB_ID_WRITE_R0           				= %h",CPU.wb_id_write_r0);
+		$display("ID_READ_DATA_1           				= %h",CPU.id_read_data_1);
+		$display("ID_READ_DATA_2           				= %h",CPU.id_read_data_2);
+		$display("ID_ALU_OP				   				= %h",CPU.id_alu_op);
+		$display("ID_ZERO_EXTENDED_IMMEDIATE			= %h",CPU.id_zero_extended_immediate);
+		$display("ID_EX_DATA_MEMORY_WRITE_CONTROL		= %h",CPU.id_ex_data_memory_write_control);
+		$display("ID_EX_DATA_MEMORY_BYTE_ENABLE_CONTROL	= %h",CPU.id_ex_data_memory_byte_enable_control);
+		$display("ID_EX_REGISTER_WRITE_CONTROL			= %h",CPU.id_ex_register_write_control);
+		$display("ID_PC_BRANCH_RESULT					= %h",CPU.id_pc_branch_result);
+		
+		
+		
+		
+		
     $display("-----EX-----");
     
     
