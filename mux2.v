df16985c160cc270b1da8d5a13240fb0cc39c430
Hpayne99 <82551168+Hpayne99@users.noreply.github.com>
2021-05-02 16:40:27 -0700

    Update mux2.v
    parameterize mux2

--- mux2.v
+++ mux2.v
@@ -1,6 +1,7 @@
-module mux2(input [15:0] in1,in2, 
+module mux2 #(parameter N = 16)(
+			input [N-1:0] in1,in2, 
 			input s,
-			output reg [15:0] out);
+			output reg [N-1:0] out);
 	
 	always@(*)
 		if (!s)
