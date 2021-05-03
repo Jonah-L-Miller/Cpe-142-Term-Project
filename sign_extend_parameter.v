1af9aff38983903137cff4997dad67689756af6f
Hpayne99 <82551168+Hpayne99@users.noreply.github.com>
2021-05-02 19:00:07 -0700

    Update sign_extend_parameter.v
    fixed errors

--- sign_extend_parameter.v
+++ sign_extend_parameter.v
@@ -2,10 +2,12 @@ module sign_extend #(parameter N = 12)(
 	input [N-1:0] in,
 	output reg [15:0] out
 	);
-	reg [0:0] one;	
+	
+	reg [32:0] temp = 32'h 11111111;
+	
 	always@(*) begin
-		one = 1;
-		out = {{16,one}, in};
+		temp = {16'h 1111, in};
+		out = temp [15:0];
 	end
 	
 endmodule
