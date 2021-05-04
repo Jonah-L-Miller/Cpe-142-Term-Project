17b7fba3c2532d0b689a479d8dd03184e1cd36af
Jonah-L-Miller <81546398+Jonah-L-Miller@users.noreply.github.com>
2021-05-04 11:27:11 -0700

    Add files via upload

--- hazard_unit.v
+++ hazard_unit.v
@@ -1,19 +1,19 @@
 module hazard_unit(		
 		input ex_memread,
 		input [3:0] id_op1, id_op2, ex_op1, ex_op2,
-		output reg id.flush, pc_pause, if_id_flush	
+		output reg id_flush, pc_pause, if_id_flush	
 		);
 
 	always@(*)
 		
-		if ( ((ex_op1 = id_op1) || (ex_op2 = id_op2)) && ( ex_memread == 1 ) ) begin
-			id.flush = 1; // flushed control signals for ID/EX buffer
+		if ( ((ex_op1 == id_op1) || (ex_op2 == id_op2)) && ( ex_memread == 1 ) ) begin
+			id_flush = 1; // flushed control signals for ID/EX buffer
 			pc_pause = 1;
 			if_id_flush = 1;
 		end
 		
 		else  begin
-			id.flush = 0; 
+			id_flush = 0; 
 			pc_pause = 0;
 			if_id_flush = 0;
 		end
